c5637770ce205579d32ec294de45b1204c0eb8da
nikhilsingh3112 <89055639+nikhilsingh3112@users.noreply.github.com>
2021-10-28 01:33:23 -0700

    Add files via upload

---
+++ Assignments/Assignment 5/Private_Constructor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace privateConstructorProgram
+{
+ class Program
+ {
+ public static string name;
+ public static int num;
+ // Creating private Constructor
+ // using private keyword
+ private Program()
+ {
+ Console.WriteLine("Welcome to Private Constructor");
+ }
+ // Default Constructor
+ // with parameters
+ public Program(string a, int b) {
+
+ name = a;
+ num = b;
+ }
+ public static void Main() {
+ // Here, the only default
+ 
+ Program obj2 = new Program("Ganesh", 747);
+ Console.WriteLine(Program.name + ", " + Program.num);
+ }
+
+ }
+} 
