efcf1f3a7eb55d32148a70982dbcc5264210b2da
nikhilsingh3112 <89055639+nikhilsingh3112@users.noreply.github.com>
2021-10-28 00:51:58 -0700

    Add files via upload

---
+++ Assignments/Assignment 1/Introduction to loop/do while/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;   
+
+public class DoWhile   
+
+    {   
+
+      public static void Main(string[] args)   
+
+      {   
+
+          int i = 1;   
+
+             
+
+          do{   
+
+              Console.WriteLine(i);   
+
+              i++;   
+
+          } while (i <= 50) ;   
+
+     
+
+     }   
+
+   }
