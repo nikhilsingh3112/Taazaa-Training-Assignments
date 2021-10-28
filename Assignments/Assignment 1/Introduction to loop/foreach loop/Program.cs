1a95d6046883717bc18e38411c3e7130d4ebf9a5
nikhilsingh3112 <89055639+nikhilsingh3112@users.noreply.github.com>
2021-10-28 00:53:23 -0700

    Add files via upload

---
+++ Assignments/Assignment 1/Introduction to loop/foreach loop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class foreachloop
+
+{ 
+
+    public static void Main() 
+
+    { 
+
+        Console.WriteLine("required number"); 
+
+        int[] a_array = new int[] { 1, 2, 3, 4, 5, 6, 7 }; 
+
+        foreach(int items in a_array) 
+
+        { 
+
+            Console.WriteLine(items); 
+
+        } 
+
+    } 
+
+}
