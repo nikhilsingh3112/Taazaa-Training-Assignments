03a91af34041a0832d231d144ec17716aadf3114
nikhilsingh3112 <89055639+nikhilsingh3112@users.noreply.github.com>
2021-10-28 00:56:57 -0700

    Add files via upload

---
+++ Assignments/Assignment 1/Strings/String methods/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+namespace StringApplication
+{
+class StringProg
+{
+static void Main(string[] args)
+{
+string[] starray = new string[]{"Hello i am Ganesh",
+"And i am from gorakhpur",
+"I completed my graduation from bbdec",
+"in year 2021",
+"in Cse"};
+string str = String.Join("\n", starray);
+Console.WriteLine(str);
+}
+}
+}
