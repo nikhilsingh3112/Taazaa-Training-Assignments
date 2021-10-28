aebe6d1a74fb5d3287b5bb7e0f8a744a0d12105e
nikhilsingh3112 <89055639+nikhilsingh3112@users.noreply.github.com>
2021-10-28 01:25:28 -0700

    Add files via upload

---
+++ Assignments/Assignment 4/Struct/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace ConsoleApplication {
+ 
+// Defining structure
+public struct Person
+{
+    // Declaring different data types
+    public string Name;
+    public int Age;
+    public int Weight;
+ 
+}
+ 
+class Ganesh {
+     
+    // Main Method
+    static void Main(string[] args)
+    {
+ 
+        // Declare P1 of type Person
+        Person P1;
+ 
+        // P1's data
+        P1.Name = "Ganesh Pandey";
+        P1.Age = 21;
+        P1.Weight = 80;
+ 
+        // Displaying the values
+        Console.WriteLine("Data Stored in P1 is " +
+                           P1.Name + ", age is " +
+                           P1.Age + " and weight is " +
+                           P1.Weight);
+ 
+    }
+}
+}
