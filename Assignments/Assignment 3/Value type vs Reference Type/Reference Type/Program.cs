6e53f942805efd53b603f7c17a982ad0ab2fb80c
nikhilsingh3112 <89055639+nikhilsingh3112@users.noreply.github.com>
2021-10-28 01:04:55 -0700

    Add files via upload

---
+++ Assignments/Assignment 3/Value type vs Reference Type/Reference Type/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Student{
+
+	public string StudentName { get; set; }
+	
+}
+public class Program
+{
+	public static void ChangeReferenceType(Student std2)
+	{
+		std2.StudentName   = "Ganesh";
+	}
+	
+	public static void Main()
+	{
+		Student std1 = new Student();
+		
+		std1.StudentName = "Shubham";
+		
+		ChangeReferenceType(std1);
+	
+		Console.WriteLine(std1.StudentName);
+	}
+}
---
+++ Assignments/Assignment 3/Value type vs Reference Type/Value Type/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+					
+public class Program
+{
+	static void ChangeValue(int x)
+	{
+		x =  200;
+	
+		Console.WriteLine(x);
+	}
+	
+	public static void Main()
+	{
+		 int i = 100;
+
+		Console.WriteLine(i);
+		
+		ChangeValue(i);
+		
+		Console.WriteLine(i);
+	}
+}
