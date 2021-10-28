d661d196827b3e16bceab7fea8c157c722ab3a41
nikhilsingh3112 <89055639+nikhilsingh3112@users.noreply.github.com>
2021-10-28 00:57:39 -0700

    Add files via upload

---
+++ Assignments/Assignment 1/Strings/Verbatim Strings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+  
+public class GFG {
+  
+    // Main method
+    static public void Main()
+    {
+  
+        // Creating and initializing the array
+        // here 'for' keyword is used as 
+        // an identifier by using @ symbol
+        string[] @for = {"C#", "PHP", "Java", "Python"};
+  
+                // as and for keywords is 
+                // as an identifier
+                // using @ symbol
+                foreach (string @as in @for)
+                {
+                    Console.WriteLine("Element of Array: {0}", @as);
+                }
+    }
+}
