53b0b8e3c9ebe05afb47553a0dee4f588062b7f5
nikhilsingh3112 <89055639+nikhilsingh3112@users.noreply.github.com>
2021-10-28 00:48:00 -0700

    Add files via upload

---
+++ Assignments/Assignment 9/Predicate_Deleg/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PredicateDele
+{
+    class Program
+    {
+         public static bool myfun(string mystring)
+    {
+        if (mystring.Length < 7)
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+  
+    // Main method
+    static public void Main()
+    {
+  
+        // Using predicate delegate
+        // here, this delegate takes
+        // only one parameter
+        Predicate<string> val = myfun;
+        Console.WriteLine(val("Ganesh Pandey"));
+    }
+    }
+}
