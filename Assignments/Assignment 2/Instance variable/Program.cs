dcc824b35cf97df9ab158ffb749667abdd123ed4
nikhilsingh3112 <89055639+nikhilsingh3112@users.noreply.github.com>
2021-10-28 00:34:00 -0700

    Add files via upload

---
+++ Assignments/Assignment 2/Instance variable/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Taazaa
+{
+ readonly static int oph;
+ static Taazaa() //Static constructor
+ {
+ oph=123;
+ Console.WriteLine(oph);
+ }
+ public static void Main()
+ {
+     Taazaa.oph= 
+ }
+}
---
+++ Assignments/Assignment 2/Jagged array/Program.cs
@@ -0,0 +1,26 @@
+﻿//A jagged array is an array whose elements are arrays, possibly of different sizes.
+// A jagged array is sometimes called an "array of arrays."
+
+//In c#, a jagged array can be initialized with two square brackets [][]. 
+//The first square bracket will specify the size of an array, 
+//and the second one will specify the dimension of the array, which will be stored as a value.
+public class JaggedArray  
+{  
+    public static void Main()  
+    {  
+        int[][] arr = new int[2][];// Declare the array  
+  
+        arr[0] = new int[] { 11, 21, 56, 78 };// Initialize the array          
+        arr[1] = new int[] { 42, 61, 37, 41, 59, 63 };  
+  
+        // Traverse array elements  
+        for (int i = 0; i < arr.Length; i++)  
+        {  
+            for (int j = 0; j < arr[i].Length; j++)  
+            {  
+                System.Console.Write(arr[i][j]+" ");  
+            }  
+            System.Console.WriteLine();  
+        }  
+    }  
+}  
