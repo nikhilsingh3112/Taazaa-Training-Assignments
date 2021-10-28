f59c3ec3f02706b2ad0a9e6b62c30603e5675120
nikhilsingh3112 <89055639+nikhilsingh3112@users.noreply.github.com>
2021-10-28 00:46:29 -0700

    Add files via upload

---
+++ Assignments/Assignment 9/Function_Delegate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FunctionDele
+{
+    class Program
+    {
+       
+    static int Sum(int x, int y)
+    {
+        return x + y;
+    }
+
+    static void Main(string[] args)
+    {
+        Func<int,int, int> add = Sum;
+
+        int result = add(10, 10);
+
+        Console.WriteLine(result); 
+    }
+
+    }
+}
