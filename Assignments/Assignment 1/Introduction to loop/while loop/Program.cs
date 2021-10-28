48e855e4d53c5f591c4ac76e59ee5236310cb8c8
nikhilsingh3112 <89055639+nikhilsingh3112@users.noreply.github.com>
2021-10-28 00:54:21 -0700

    Add files via upload

---
+++ Assignments/Assignment 1/Introduction to loop/while loop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System; 
+
+   
+
+class whileloop 
+
+{ 
+
+    public static void Main() 
+
+    { 
+
+        int x = 1; 
+
+        while (x <= 10) 
+
+        { 
+
+            Console.WriteLine("Ganesh"); 
+
+            x++; 
+
+        } 
+
+    } 
+
+} 
