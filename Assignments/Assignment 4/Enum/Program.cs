9fc450e126f4c8d42ee450f8e4f20ac4bb086fc7
nikhilsingh3112 <89055639+nikhilsingh3112@users.noreply.github.com>
2021-10-28 01:08:27 -0700

    Add files via upload

---
+++ Assignments/Assignment 4/Enum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace MyApplication
+{
+  class Program   
+  {
+    enum Week   //An enum is a special "class" that represents a group of constants (unchangeable/read-only variables).
+    {
+      Monday,    
+      Tuesday,   
+      Wednesday,      
+      Thursday,      
+      Friday,        
+      Saturday,       
+      Sunday        
+    }
+    static void Main(string[] args)
+    {
+      int myNum = (int) Week.Friday;  //You can access enum items with the dot syntax
+      Console.WriteLine(myNum);  //To get the integer value from an item, you must explicitly convert the item to an int
+    }
+  }
+}
