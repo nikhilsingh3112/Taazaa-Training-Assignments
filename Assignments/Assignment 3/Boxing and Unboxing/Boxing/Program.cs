f4e34ed6890858cdacead153cd7927561dfd6b7d
nikhilsingh3112 <89055639+nikhilsingh3112@users.noreply.github.com>
2021-10-28 01:01:29 -0700

    Add files via upload

---
+++ Assignments/Assignment 3/Boxing and Unboxing/Boxing/Program.cs
@@ -0,0 +1,17 @@
+﻿class TestBoxing
+{
+    static void Main()
+    {
+        int i = 123;
+
+        // Boxing copies the value of i into object o.
+        object o = i;//converting a value type into reference type is boxing
+
+        // Change the value of i.
+        i = 456;
+
+        // The change in i doesn't affect the value stored in o.
+        System.Console.WriteLine("The value-type value = {0}", i);
+        System.Console.WriteLine("The object-type value = {0}", o);
+    }
+}
---
+++ Assignments/Assignment 3/Boxing and Unboxing/Unboxing/Program.cs
@@ -0,0 +1,17 @@
+﻿class TestUnboxing
+{
+    static void Main()
+    {
+        int i = 100;
+
+        
+        object o = i;//converting a value type into reference type is boxing
+        int j=(int)o;// Unboxing
+
+       
+
+        
+        System.Console.WriteLine("The value-type value = {0}", i);
+        System.Console.WriteLine("The object-type value = {0}", j);
+    }
+}
