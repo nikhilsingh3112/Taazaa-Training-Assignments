81b80d0e4f05a85aa20c67e4cf9d610eed826b60
nikhilsingh3112 <89055639+nikhilsingh3112@users.noreply.github.com>
2021-10-28 01:02:27 -0700

    Add files via upload

---
+++ Assignments/Assignment 3/Interface/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;  
+public interface Vehicle  
+{  
+    void accelerate();// by default methods are abstract  
+}  
+public class car : Vehicle  
+{  
+    public void accelerate()  
+    {  
+        Console.WriteLine("is Private property");  
+    }  
+}  
+public class bus : Vehicle  
+{  
+    public void accelerate()  
+    {  
+        Console.WriteLine("is Public property");  
+    }  
+}  
+public class TestInterface  
+{  
+    public static void Main()  
+    {  
+        car d = new car();   
+         d.accelerate();  
+        bus d1 = new bus();  
+         d1.accelerate();  
+    }  
+}
