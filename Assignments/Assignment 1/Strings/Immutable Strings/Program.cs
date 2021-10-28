7ae5460934fff18d8f85b308013cf428fb1ad368
nikhilsingh3112 <89055639+nikhilsingh3112@users.noreply.github.com>
2021-10-28 00:55:24 -0700

    Add files via upload

---
+++ Assignments/Assignment 1/Strings/Immutable Strings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StringApplication {
+
+   class StringProg {
+
+      static void Main(string[] args) {
+         string str1 = "ganesh";
+         string str2 = "shubham";
+
+         if (String.Compare(str1, str2) == 0) {
+            Console.WriteLine(str1 + " and " + str2 + " are equal strings.");
+         } else {
+            Console.WriteLine(str1 + " and " + str2 + " are not equal strings.");
+         }
+         Console.ReadKey() ;
+      }
+   }
+}
