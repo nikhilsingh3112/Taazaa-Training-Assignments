acd09bdee058420dc4cf120b0434bd58d274b286
nikhilsingh3112 <89055639+nikhilsingh3112@users.noreply.github.com>
2021-10-28 00:43:26 -0700

    Add files via upload

---
+++ Assignments/Assignment 9/Exception_Handling/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main()
+        {
+              try  
+        {  
+            int a = 10;  
+            int b = 0;  
+            int x = a / b;  
+        }  
+        catch (Exception e) 
+        
+        { 
+            Console.WriteLine(e); 
+        }  
+  
+        Console.WriteLine("Rest to ");  
+        }
+    }
+}
