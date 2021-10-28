9061e28019a20bbd249502281f4fbee46ee3ca1e
nikhilsingh3112 <89055639+nikhilsingh3112@users.noreply.github.com>
2021-10-28 00:52:40 -0700

    Add files via upload

---
+++ Assignments/Assignment 1/Introduction to loop/For loop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;   
+
+public class forloopprog  
+
+    {   
+
+      public static void Main()   
+
+      {   
+
+          for(int i=1;i<=10;i++){     
+
+            Console.WriteLine(i);     
+
+          }     
+
+      }   
+
+    }   
