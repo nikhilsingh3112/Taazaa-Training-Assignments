4e2fd055e8b9801bb40c1c424b82175c2ab66a7e
nikhilsingh3112 <89055639+nikhilsingh3112@users.noreply.github.com>
2021-10-28 01:00:22 -0700

    Add files via upload

---
+++ Assignments/Assignment 3/Abstract class/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+abstract class Animal
+{
+  // Abstract method (does not have a body)
+  public abstract void animalSound();
+  public void sleep()
+  {
+    Console.WriteLine("sleeping");
+  }
+}
+class horse : Animal
+{
+  public override void animalSound()
+  {
+    Console.WriteLine("horse class function");
+  }
+}
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    horse h1 = new horse(); 
+    h1.animalSound();  
+    h1.sleep();  
+  }
+}
